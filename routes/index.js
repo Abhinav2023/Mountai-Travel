cf8cee0e2acbe5576b64f900a92e90974fdc2ecb
Abhinav Bansal <abhinavbansal231101@gmail.com>
2019-08-04 08:26:11 +0000

    vavh

--- routes/index.js
+++ routes/index.js
@@ -111,7 +111,7 @@ router.post('/forgot', function(req, res, next) {
         subject: 'Node.js Password Reset',
         text: 'You are receiving this because you (or someone else) have requested the reset of the password for your account.\n\n' +
           'Please click on the following link, or paste this into your browser to complete the process:\n\n' +
-          'https://' + 'sleepy-hollows-51902.herokuapp.com/' + '/reset/' + token + '\n\n' +
+          'https://' + 'sleepy-hollows-51902.herokuapp.com' + '/reset/' + token + '\n\n' +
           'If you did not request this, please ignore this email and your password will remain unchanged.\n'
       };
       smtpTransport.sendMail(mailOptions, function(err) {
